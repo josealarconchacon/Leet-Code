6d003faf57761c69951febe80d33708fdf3725dd
josealarconchacon <jose.alarcon.chacon@gmail.com>
2022-03-30 11:32:02 -0400

    Two Sum

---
+++ JS/TwoSum/twoSum.js
@@ -0,0 +1,30 @@
+"use strict";
+
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+/*
+    Example 1:
+    Input: nums = [2,7,11,15], target = 9
+    Output: [0,1]
+    Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+    Example 2:
+    Input: nums = [3,2,4], target = 6
+    Output: [1,2]
+
+    Example 3:
+    Input: nums = [3,3], target = 6
+    Output: [0,1]
+ */
+
+const twoSum = function (num, target) {
+  for (let i = 0; i < num.length; i++) {
+    for (let j = i + 1; j < num.length; j++) {
+      if (num[i] + num[j] == target) {
+        return [i, j];
+      }
+    }
+  }
+};
+console.log(twoSum([3, 2, 4], 6)); // [1,2]
+console.log(twoSum([2, 7, 11, 15], 9)); // [0,1]
+console.log(twoSum([3, 3], 6));

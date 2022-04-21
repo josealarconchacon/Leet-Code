891cf774389da25b43a1bf82c852d49000d699e8
josealarconchacon <jose.alarcon.chacon@gmail.com>
2022-04-21 12:13:31 -0400

    test VIPCustomer class

--- Java/Constructor/src/com/company/Main.java
+++ Java/Constructor/src/com/company/Main.java
@@ -15,5 +15,17 @@ public class Main {
 
         bobsAccount.deposit(51.0);
         bobsAccount.withdrawal(100.0);
+
+        System.out.println("---------- VIPCustomer ---------------");
+        VIPCustomer P1 = new VIPCustomer();
+        System.out.println(P1.getName());
+
+        VIPCustomer P2 = new VIPCustomer("Pablo", 50000.00);
+        System.out.println(P2.getName());
+        System.out.println(P2.getEmail());
+
+        VIPCustomer P3 = new VIPCustomer("Tim", 200000.00, "tim@gmail.com");
+        System.out.println(P3.getName());
+        System.out.println(P3.getEmail());
     }
 }

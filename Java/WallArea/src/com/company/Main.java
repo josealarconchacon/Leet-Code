2753390ffae91da57998f2488b24a85f72490914
josealarconchacon <jose.alarcon.chacon@gmail.com>
2022-05-03 10:29:29 -0400

    tester for Point class

--- Java/WallArea/src/com/company/Main.java
+++ Java/WallArea/src/com/company/Main.java
@@ -1,8 +1,16 @@
 package com.company;
 
+import java.awt.*;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    sPoint p1 = new sPoint(6,5);
+        sPoint p2 = new sPoint(3,1);
+
+        System.out.println("Distance (0,0) = " + p1.distance());
+//        System.out.println("Distance (p2) = " + p1.distance(p2));
+        System.out.println("Distance(2,2) = " + p1.distance(2,2));
+
     }
 }

de7c41d5796dfbe4b145451b4c190c930c653c3d
josealarconchacon <jose.alarcon.chacon@gmail.com>
2022-05-03 14:53:03 -0400

    inheritance

---
+++ Java/Animal/src/com/company/Cat.java
@@ -0,0 +1,39 @@
+package com.company;
+// mammal
+public class Cat extends Animal {
+    private boolean dangerous;
+
+    public Cat() {
+        super();
+        this.dangerous = false;
+    }
+
+    public Cat(String C, int A, boolean D) {
+        // construct the parent object Animal
+        super("Mammal", C, A);
+        this.dangerous = D;
+    }
+
+    public String toString() {
+        return  super.toString() + ", " + this.dangerous;
+    }
+
+    public boolean equal(Cat C) {
+        if(super.equal(C) && !this.dangerous) {
+            return true;
+        }
+        return false;
+    }
+    @Override
+    public void speak() {
+        System.out.println("Meow!");
+    }
+
+    public boolean isDangerous() {
+        return dangerous;
+    }
+
+    public void setDangerous(boolean dangerous) {
+        this.dangerous = dangerous;
+    }
+}
--- Java/Animal/src/com/company/Main.java
+++ Java/Animal/src/com/company/Main.java
@@ -5,7 +5,10 @@ public class Main {
     public static void main(String[] args) {
         Animal A1 = new Dog();
         System.out.println("" + A1);
-
         A1.speak();
+
+        Animal A2 = new Cat();
+        System.out.println("" + A2);
+        A2.speak();
     }
 }

bcdb0ffb1cabe3c9a9293e9ac348abedd9f6f380
josealarconchacon <jose.alarcon.chacon@gmail.com>
2022-06-02 14:35:25 -0400

    game

--- Java/MAC190/FReview/FReview/src/BallGame.java
+++ Java/MAC190/FReview/FReview/src/BallGame.java
@@ -1,47 +1,127 @@
 public class BallGame {
     private String FirstTeam, SecondTeam;
-    private int NumOfPlayerInEachTeam;
-    private int NumOfSubstitutes;
-    private int Score;
+    private int NumOfPlayerInEachTeam, NumOfSubstitutes;
+    private int FirstTeamScore, SecondTeamScore;
 
     // Provide a default constructor
     public BallGame() throws Exception {
-        this("Unknown","Unknown",0,0,0);
+        this("Unknown","Unknown",0,0,0,0);
     }
 
-    public BallGame(String firstTeam, String secondTeam, int numOfPlayerInEachTeam, int numOfSubstitutes, int score) throws Exception {
+    // Provide a constructor that accepts specific values for each of the private members.
+    // Throw an exception (of your design) if the numbers are negative.
+    public BallGame(String firstTeam, String secondTeam, int numOfPlayerInEachTeam, int numOfSubstitutes, int firstTeamScore, int secondTeamScore) throws Exception {
+        this.FirstTeam = firstTeam;
+        this.SecondTeam = secondTeam;
+        if(numOfPlayerInEachTeam <= 0) {
+            throw new Exception("Number of Player can't be zero or negative number");
+        }
+        if(numOfSubstitutes <= 0) {
+            throw new Exception("Max Substitutes can't be negative");
+        }
+        if(firstTeamScore < 0 || secondTeamScore < 0) {
+            throw new Exception("Score for both teams can't be negative");
+        }
+        this.NumOfPlayerInEachTeam = numOfPlayerInEachTeam;
+        this.NumOfSubstitutes = numOfSubstitutes;
+        this.FirstTeamScore = firstTeamScore;
+        this.SecondTeamScore = secondTeamScore;
     }
 
+    // Provide a method TheFirstTeamScore and TheSecondTeamScore that increases the score of the appropriate team by a specified value
+    // (i.e TheFirstTeamScore and TheSecondTeamScore accepts an integer). Throw an exception if the values are negative.
+    public void TheFirstTeamScore(int n) throws Exception {
+        if(n < 0) {
+            throw new Exception("Score can't be negative");
+        }
+        this.FirstTeamScore += n;
+    }
+    public void TheSecondTeamScore(int n) throws Exception {
+        if(n < 0) {
+            throw new Exception("Score can't be negative");
+        }
+        this.SecondTeamScore += n;
+    }
+
+    // A method display that displays all attributes of an object
+    public void Display() {
+        System.out.println("First Team = " + this.FirstTeam + "\n" +
+                            "Second Team = " + this.SecondTeam + "\n" +
+                            "Num of Player = " + this.NumOfPlayerInEachTeam + "\n" +
+                           " Num of Substitutes = " + this.NumOfSubstitutes + "\n" +
+                           " First Team Score = " + this.FirstTeamScore + "\n" +
+                           " Second Team Score = " + this.SecondTeamScore);
+    }
+
+
+    //Provide a method Equal that accepts an object BallGame and returns true if the object is equal to current object and false otherwise.
+    public boolean equals(BallGame ballGame) {
+        if(this.FirstTeam.equals(ballGame.FirstTeam) && this.SecondTeam.equals(ballGame.SecondTeam) &&
+            this.NumOfPlayerInEachTeam == ballGame.NumOfPlayerInEachTeam && this.NumOfSubstitutes == ballGame.NumOfSubstitutes &&
+            this.FirstTeamScore == ballGame.FirstTeamScore && this.SecondTeamScore == ballGame.SecondTeamScore) {
+            return true;
+        }
+        return false;
+    }
+
+    // Provide a method WhoIsWinning that returns the name of the winning team or “Draw” if both have same score.
+    public String WhoIsWinning() {
+        if(this.FirstTeamScore > this.SecondTeamScore) {
+            return FirstTeam;
+        } else if(this.SecondTeamScore > this.FirstTeamScore) {
+            return SecondTeam;
+        } else {
+            return "Draw";
+        }
+    }
 
     // Provide accessors for each of the members
     public String getFirstTeam() {
         return FirstTeam;
     }
-    public void setFirstTeam(String firstTeam) throws Exception{
+    public void setFirstTeam(String firstTeam) {
         FirstTeam = firstTeam;
     }
     public String getSecondTeam() {
         return SecondTeam;
     }
-    public void setSecondTeam(String secondTeam) throws Exception{
+    public void setSecondTeam(String secondTeam) {
         SecondTeam = secondTeam;
     }
     public int getNumOfPlayerInEachTeam() {
         return NumOfPlayerInEachTeam;
     }
     public void setNumOfPlayerInEachTeam(int numOfPlayerInEachTeam) throws Exception{
+        if(numOfPlayerInEachTeam <= 0) {
+            throw new Exception("Number of players can't be zero of negative");
+        }
         NumOfPlayerInEachTeam = numOfPlayerInEachTeam;
     }
     public int getNumOfSubstitutes() {
         return NumOfSubstitutes;
     }
     public void setNumOfSubstitutes(int numOfSubstitutes) throws Exception{
+        if(numOfSubstitutes <= 0) {
+            throw new Exception("Max Substitutes can't be negative");
+        }
         NumOfSubstitutes = numOfSubstitutes;
     }
-    public int getScore() {
-        return Score;
+    public int getFirstTeamScore() {
+        return FirstTeamScore;
     }
-    public void setScore(int score) throws Exception{
-        Score = score;
+    public void setFirstTeamScore(int firstTeamScore) throws Exception{
+        if(firstTeamScore <= 0) {
+            throw new Exception("Score for firstTeamScore can't be negative");
+        }
+        FirstTeamScore = firstTeamScore;
+    }
+    public int getSecondTeamScore() {
+        return SecondTeamScore;
+    }
+    public void setSecondTeamScore(int secondTeamScore) throws Exception{
+        if(secondTeamScore <= 0) {
+            throw new Exception("Score for secondTeamScore can't be negative");
+        }
+        SecondTeamScore = secondTeamScore;
     }
 }
---
+++ Java/MAC190/FReview/FReview/src/SoccerGame.java
@@ -0,0 +1,94 @@
+//Derive a class SoccerGame that has in addition the following:
+public class SoccerGame extends BallGame{
+    private String Referee;
+    private int SubstitutedA, SubstitutedB;
+
+    // - Provide a default constructor that sets team names and referee’s name to “unknown”,
+    // number of payers to 11, number of substitutes to 5 and the score of each team as well as number of players substituted to 0.
+    public SoccerGame() throws Exception {
+        super("Unknown","Unknown",11,5,0,0);
+        this.Referee = "Unknown";
+        this.SubstitutedA = this.SubstitutedB = 0;
+    }
+    
+    // - Provide a constructor that accepts the names of the teams and the referee’s name.
+    public SoccerGame(String firstTeam, String secondTeam, int numOfPlayerInEachTeam, int numOfSubstitutes, int firstTeamScore, int secondTeamScore, int substitutedA, int substitutedB, String referee) throws Exception {
+        super(firstTeam, secondTeam, numOfPlayerInEachTeam, numOfSubstitutes, firstTeamScore, secondTeamScore);
+        this.Referee = referee;
+        this.SubstitutedA = substitutedA;
+        this.SubstitutedB = substitutedB;
+    }
+
+    // - Override the method display to display all private attributes.
+    public void Display() {
+        super.Display();
+        System.out.println("Referee = " + this.Referee + " SubstitutedA = " + this.SubstitutedA + " SubstitutedB = " + this.SubstitutedB);
+    }
+
+    // - Override the method Equal to compare two SoccerGames
+    public boolean equals(SoccerGame soccerGame) {
+        if(super.equals(soccerGame) && this.Referee.equals(soccerGame.getReferee()) && this.SubstitutedA == soccerGame.SubstitutedA &&this.SubstitutedB == soccerGame.SubstitutedB) {
+            return true;
+        }
+        return false;
+    }
+
+    // - Override  TeamAScored and TeamBScored that accepts an integer to increase the score of the appropriate team by 1 even if the number specified is larger than 1.
+    public void TeamAScored(int n) throws Exception {
+        if(n < 0) {
+            throw new Exception("Can't be negative");
+        }
+        this.setFirstTeamScore(this.getFirstTeamScore() + 1);
+    }
+    public void TeamBScored(int n) throws Exception {
+        if(n < 0) {
+            throw new Exception("Can't be negative");
+        }
+        this.setSecondTeamScore(this.getSecondTeamScore() + 1);
+    }
+
+    // Provide a method SubstitutionA, SubstitutionB that increases the number of players substituted for the appropriate team by 1 if it is less than 3,
+    // otherwise it displays “No more substitutions allowed”.
+    public void SubstitutionA() {
+        if(SubstitutedA < this.getNumOfSubstitutes()) {
+            this.SubstitutedA += 1;
+        } else {
+            System.out.println("No more substitution allowed");
+        }
+    }
+    public void SubstitutionB() {
+        if(SubstitutedB < this.getNumOfSubstitutes()) {
+            this.SubstitutedB += 1;
+        } else {
+            System.out.println("No more substitution allowed");
+        }
+    }
+
+    public String getReferee() {
+        return Referee;
+    }
+    public void setReferee(String referee) {
+        Referee = referee;
+    }
+    public int getSubstitutedA() {
+        return SubstitutedA;
+    }
+
+    public void setSubstitutedA(int substitutedA) throws Exception{
+        if(substitutedA < 0 || substitutedA > this.getNumOfSubstitutes()) {
+            throw new Exception("Invalid number of substituted");
+        }
+        SubstitutedA = substitutedA;
+    }
+
+    public int getSubstitutedB() {
+        return SubstitutedB;
+    }
+
+    public void setSubstitutedB(int substitutedB) throws Exception{
+        if(substitutedB < 0 || substitutedB > this.getNumOfSubstitutes()) {
+            throw new Exception("Invalid number of substituted");
+        }
+        SubstitutedB = substitutedB;
+    }
+}
